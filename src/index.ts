836a7336c8bcc72c5c66779e9c5bb8dee6e5eeb1
別府大地 <beppudaichi@beppudaichis-MacBook-Air.local>
2022-04-07 15:35:26 +0900

    mysqlをインストールした

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,31 @@
 import  express  from "express"
+import * as mysql from "mysql";
 
 const app = express()
 const port = process.env.PORT || 3001
 
+const connection = async () => {
+  return await mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password:'',
+  });
+};
+
 app.get('/', (req: express.Request, res: express.Response) => {
-  res.send('tsだお')
+  try{
+  connection()
+    .then((connection) => {
+      const result = connection.query("SELECT * FROM MOVIE");
+      connection.end();
+      return result;
+    })
+    .then(function (rows) {
+      res.send(rows)
+    })
+  } catch(e){
+    console.log('error', e)
+  }
 })
 
 app.listen(port, () => {
