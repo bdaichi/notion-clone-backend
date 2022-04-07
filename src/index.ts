1486b6397719d878b43eaa513af26438edc94b94
別府大地 <beppudaichi@beppudaichis-MacBook-Air.local>
2022-04-07 17:55:17 +0900

    フロントエンドと接続できるようになった

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,33 @@
 import  express  from "express"
 import * as mysql from "mysql";
-
+import path from "path";
+ 
 const app = express()
 const port = process.env.PORT || 3001
-
-const connection = async () => {
-  return await mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password:'',
-  });
-};
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, '../frontend-react/*')));
+// const connection = async () => {
+//   return await mysql.createConnection({
+//     host: 'localhost',
+//     user: 'root',
+//     password:'',
+//   });
+// };
 
 app.get('/', (req: express.Request, res: express.Response) => {
-  try{
-  connection()
-    .then((connection) => {
-      const result = connection.query("SELECT * FROM MOVIE");
-      connection.end();
-      return result;
-    })
-    .then(function (rows) {
-      res.send(rows)
-    })
-  } catch(e){
-    console.log('error', e)
-  }
+  res.send('tsこんにちは')
 })
+
+app.get("/api", (req, res) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000')
+  res.status(201)
+  res.json({ message: "Hello World!" });
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend-react/pages/index.tsx'));
+});
 
 app.listen(port, () => {
   console.log(`listening on *:${port}`);
