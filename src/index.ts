972a2561384f45cfd81bb30e07afddc0a12c39d2
別府大地 <beppudaichi@beppudaichis-MacBook-Air.local>
2022-04-14 11:13:43 +0900

    corsのやろうに邪魔されてデータを取得できない

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from "express"
 import axios from "axios";
+import bodyParser from "body-parser";
 import path from "path";
 
 import createUserData from "./mysql/user_mysql/create_user_data";
@@ -7,37 +8,23 @@ import readUserData from "./mysql/user_mysql/read_user_data";
  
 const app = express()
 const port = process.env.PORT || 3001
-const axiosBase =  axios.create({
-  baseURL: 'http://localhost:3001',
-  headers: {
-    'Content-Type': 'application/json',
-    'X-Requested-With': 'XMLHttpRequest',
-    'Access-Control-Allow-Origin': 'http://localhost:3000'
-  },
-  responseType: 'json'  
-})
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, '../../frontend-react/*')));
 
 app.get('/read_api', (req, res) => {
   readUserData(res, req)
 });
 
- app.get('/create_api', (req, res) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000')
+app.post('/post_api', (req, res) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', '*')
   res.status(201)
-  
-    axiosBase.get('http://localhost:3000')
-      .then(function (response) {
-        console.log('fetch_front_data', response)
-      })
-    .catch(function (error) {
-      // handle error
-      console.log(error);
-  })
-  // createUserData(req, res, 'daichihogehoge@icloud.com', 'mame')
+  console.log('fetch_front_data', req.body.patams)
+  res.json({
+    msg: "success",
+});
 })
 
 app.listen(port, () => {
