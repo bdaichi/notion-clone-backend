a05929083c199f7be703a086f7fae8ce64b61202
別府大地 <beppudaichi@beppudaichis-MacBook-Air.local>
2022-04-19 09:47:17 +0900

    contentをhostPageIdを指定して取得できるようになった

--- src/mysql/content_mysql.ts
+++ src/mysql/content_mysql.ts
@@ -4,7 +4,7 @@ import { RowDataPacket } from "mysql2";
 export async function ReadContensData(res: any, req: any){
     connection.connect()
     connection.query(
-        "SELECT * FROM contents WHERE contentId='bfb08fc7-4c18-4876-9889-39156a89a322'",
+        "SELECT * FROM contents WHERE hostPageId='bfb08fc7-4c18-4876-9889-39156a89a322'",
         function(err, results: RowDataPacket, fields) {
         if(err) {
             console.log("接続終了(異常)");
