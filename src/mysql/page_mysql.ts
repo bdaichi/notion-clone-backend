adcba3271160158601b8d9ae8d8838a863efa6ec
別府大地 <beppudaichi@beppudaichis-MacBook-Air.local>
2022-04-20 09:22:53 +0900

    ログイン必須にしたからこっちも+userIdで取得する

--- src/mysql/page_mysql.ts
+++ src/mysql/page_mysql.ts
@@ -20,7 +20,7 @@ export async function CreatePageData() {
 export async function ReadOriginallyPagesData(res: any, req: any){
     connection.connect()
     connection.query(
-        "SELECT * FROM pages WHERE pageId in ('quickMemo', 'taskManagement', 'tryUsing')",
+        "SELECT * FROM pages WHERE pageId in ('quickMemo', 'taskManagement', 'tryUsing'), userId='daichi'",
         function(err, results: RowDataPacket, fields) {
         if(err) {
             console.log("接続終了(異常)");
